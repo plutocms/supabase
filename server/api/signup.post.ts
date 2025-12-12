8f22de1d90affff5b72febf6bc5f240c35c7357a
Victor Ribeiro <devjvribeiro@gmail.com>
2025-12-12 13:35:50 -0300

    fix: handle sign-up errors and return user data on successful registration

--- server/api/signup.post.ts
+++ server/api/signup.post.ts
@@ -5,17 +5,24 @@ export default defineEventHandler(async (event) => {
 
   const body = await readBody(event)
 
-  await client.auth.signUp({
+  const { data, error } = await client.auth.signUp({
     email: body.email,
     password: body.password,
     options: {
       data: {
         first_name: body.first_name,
         last_name: body.last_name,
-        is_admin: false,
       },
     },
   })
 
-  return { message: 'User registered successfully' }
+  if (error) {
+    throw createError({
+      statusCode: Number.parseInt(error.code ?? '500'),
+      cause: error.cause,
+      message: error.message,
+    })
+  }
+
+  return { message: 'User registered successfully', data }
 })
