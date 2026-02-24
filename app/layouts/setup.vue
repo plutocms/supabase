7a4a95da1cc27bcdf3966752c00e81beffc27463
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-24 12:39:38 -0300

    feat: added base setup layout

---
+++ app/layouts/setup.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="dark:bg-admin-content light:bg-white font-outfit flex h-full grow flex-col"
+  >
+    <div class="flex h-full justify-center">
+      <div class="grow overflow-hidden">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
