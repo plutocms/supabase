5c679e31ed5e66704af3560c8c58213cb2222284
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-26 17:32:56 -0300

    fix: redirect to Wizard if fetch fails

--- app/middleware/setup-check.ts
+++ app/middleware/setup-check.ts
@@ -1,21 +1,25 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   if (to.path !== '/admin/setup' && to.path.startsWith('/admin')) {
-    const data = await $fetch('/api/settings/first_setup')
-
-    if (
-      data.is_first_setup === true &&
-      from.path.startsWith('/admin') &&
-      to.path !== '/admin/setup'
-    ) {
+    try {
+      const data = await $fetch('/api/settings/first_setup')
+  
+      if (
+        data.is_first_setup === true &&
+        from.path.startsWith('/admin') &&
+        to.path !== '/admin/setup'
+      ) {
+        return navigateTo('/admin/setup')
+      }
+  
+      if (
+        data.is_first_setup === false &&
+        from.path.startsWith('/admin') &&
+        to.path === '/admin/setup'
+      ) {
+        return navigateTo('/')
+      }
+    } catch(error) {
       return navigateTo('/admin/setup')
-    }
-
-    if (
-      data.is_first_setup === false &&
-      from.path.startsWith('/admin') &&
-      to.path === '/admin/setup'
-    ) {
-      return navigateTo('/')
     }
   }
 })
