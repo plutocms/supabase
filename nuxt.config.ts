5c17129eed69ce9628c4d16d4c222040007ed393
Victor Ribeiro <devjvribeiro@gmail.com>
2026-03-01 16:58:35 -0300

    chore: removed nuxt/ui module as is comes from pluto layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ const isDev = cwd.includes('supabase')
 export default defineNuxtConfig({
   extends: ['github:plutocms/pluto', 'github:plutocms/utils'],
 
-  modules: ['@nuxt/eslint', '@nuxtjs/supabase', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxtjs/supabase'],
 
   $meta: {
     name: 'supabase',
