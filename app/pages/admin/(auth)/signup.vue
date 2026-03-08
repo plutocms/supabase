de762bf1b7e3d9c933ad7f6d0e9a2adb577b7124
Victor Ribeiro <devjvribeiro@gmail.com>
2026-03-08 04:10:31 -0300

    feat: enhance signup flow with email verification message and user guidance

--- app/pages/admin/(auth)/signup.vue
+++ app/pages/admin/(auth)/signup.vue
@@ -23,12 +23,15 @@ const passwordMatch = computed(() => {
   return form.value.password === form.value.confirm_password
 })
 
+const isEmailVerificationMessageVisible = ref<boolean>(false)
+
 async function submitForm() {
   if (!passwordMatch.value) {
     return
   }
 
   isSubmitting.value = true
+
   try {
     await $fetch('/api/signup', {
       method: 'POST',
@@ -67,7 +70,41 @@ async function submitForm() {
 <template>
   <div class="grid h-full place-items-center">
     <UCard class="w-100">
-      <form @submit.prevent="submitForm">
+      <div v-if="isEmailVerificationMessageVisible" class="space-y-6 py-10">
+        <div class="text-center space-y-4">
+          <hgroup class="flex flex-col gap-y-1">
+            <span class="text-5xl">
+              <UIcon name="lucide:mail-warning" class="inline-block" />
+            </span>
+
+            <h1 class="text-3xl font-bold">Almost there!</h1>
+          </hgroup>
+
+          <p>
+            A verification link has been sent to your email. Please check your
+            inbox and follow the instructions to verify your account.
+          </p>
+
+          <UAlert icon="lucide:check" color="success" variant="outline">
+            <template #description>
+              You can close this page. You will be logged in automatically once
+              you verify your email.
+            </template>
+          </UAlert>
+
+          <UAlert
+            icon="lucide:alert-triangle"
+            color="warning"
+            variant="outline"
+          >
+            <template #description>
+              If you don't see the email, please check your spam folder.
+            </template>
+          </UAlert>
+        </div>
+      </div>
+
+      <form v-else @submit.prevent="submitForm">
         <div class="flex flex-col gap-y-6">
           <h1 class="text-3xl font-bold">Create Account</h1>
 
